888dae925e18158e694f20c7f638043d2037b29a
AlexSoftInc-php <58146921+AlexSoftInc-php@users.noreply.github.com>
2020-07-24 06:31:55 +0500

    Create index.php

---
+++ cool_query_for_b24_work/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Form update data</title>
+</head>
+<body>
+	<p>Чуть позже накину Bootstrap пока надо отработать функционал</p>
+	<form action="upd.php" method="post">
+		<label>ID старого кабинета</label> &nbsp; <input type="text" name="last_id"><br/>
+     	<label>ID нового кабинета</label> &nbsp; <input type="text" name="new_id"><br/>
+	<hr>	<button type="submit">Перенести заказы и бонусы</button>
+	</form>
+</body>
+</html>
